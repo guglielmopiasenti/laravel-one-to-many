e6c689b98513a6528afda8fd375aa334827ea120
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-07 17:20:39 +0200

    seeder updated

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,14 @@ class TypeSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        $labels = ['FrontEnd', 'BackEnd', 'FullStack', 'UI/UX', 'Design'];
+        foreach($labels as $label){
+            $type = new Type();
+            $type->label = $label;
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
     }
 }
